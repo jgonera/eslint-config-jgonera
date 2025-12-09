d6cf20c37c65559fabf8a4f3519628c8c595b014
Juliusz Gonera <jgonera@gmail.com>
2025-12-08 19:19:56 -0800

    Disable indent rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,6 +17,8 @@ export default defineConfig(
     },
     plugins: { "simple-import-sort": simpleImportSort },
     rules: {
+      // Let Prettier handle all indentation
+      indent: "off",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       "@typescript-eslint/restrict-template-expressions": [

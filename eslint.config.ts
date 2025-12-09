a752793baba16a33706d5647be303d77a329e16e
Juliusz Gonera <jgonera@gmail.com>
2025-12-08 18:49:33 -0800

    Update packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,10 @@
 import eslint from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
